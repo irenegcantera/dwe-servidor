fb98b55b34d1d850b86ae9104cc75a24eac4a3d5
Irene González Cantera <irenegcantera@gmail.com>
2021-10-24 00:08:30 +0200

    FUNC-002 created mesYearActual() function

--- calendario/funciones.php
+++ calendario/funciones.php
@@ -3,12 +3,11 @@
 <?php
 
 // función que crea una tabla que contendrá los días del mes
-function crearTablaMes($dia,$mes,$year){
-
-    echo "<table style='border: 1px solid black;'>";
+function crearTablaMes(){
+    echo "<table>";
         echo "<caption>";
             echo "<a> << </a>";
-            echo "&nbsp;&nbsp;$mes $year&nbsp;&nbsp;";
+            echo "&nbsp;&nbsp;". mesYearActual()."&nbsp;&nbsp;";
             echo "<a> >> </a>";
         echo "</caption>";
         echo "<tr>";
@@ -20,7 +19,22 @@ function crearTablaMes($dia,$mes,$year){
         echo "</table>";
 }
 
+
+// " . $_SERVER['PHP_SELF'] . "?a=" . ++$a . "
+
+// función que devuelve el mes actual y año actual
+function mesYearActual(){
+    $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    return $meses[date('n')-1]." ".date("Y");
+}
+
 // función que calcula el mes siguiente y el mes anterior
+// function mesSiguiente(){
+//     $fecha = date('Y-m-j');
+//     $nuevafecha = strtotime ( '+1 month' , strtotime ( $fecha ) ) ;
+//     $nuevafecha = date ( 'Y-m-j' , $nuevafecha );
+//     echo $nuevafecha;
+// }
 
 // función que lee datos de un archivo
 

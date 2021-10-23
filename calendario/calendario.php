6e0aa1134b0e9ad8dbe13e331f9d122c20d0adc7
Irene González Cantera <irenegcantera@gmail.com>
2021-10-24 00:07:33 +0200

    CAL-003 without major changes

--- calendario/calendario.php
+++ calendario/calendario.php
@@ -10,44 +10,29 @@
 <body>
     <h1>CALENDARIO</h1>
 
-
     <?php 
     require ('funciones.php');
-    if(isset($_GET['dia'])){
-        if(isset($_GET['mes'])){
-            if(isset($_GET['year'])){
-                $dia = $_GET['dia'];
-                $mes = $_GET['mes'];
-                $year = $_GET['year'];
-                crearTablaMes($dia,$mes,$year);
-            }else{
-                echo "NADA";
-            }
-        }else{
-            echo "NADA";
-        }
-    }else{
-        echo "NADA";
-    }
-
-
-
-    
+    crearTablaMes();
+    // if(isset($_GET['tareas'])){
+                
+    // }else{
+    //     echo "NADA";
+    // }
     // funcion que te dice dia, mes y año actual, esto son los hidden 
     // se pasan por parámetro en crear tabla 
     // y se coloca en echo
     // otra funcion que calcule mes siguiente y otra mes anterior y te lo saca solo
     ?>
-
+    
     <form name = "form" action="" method="GET">
         <br>
         <strong>Citas del día:</strong>
         <br>
         <textarea name="tareas" rows="5" cols="54" autofocus></textarea>
         <br>
-        <input name="dia" type="hidden" value="<?php echo date("j");?>"/>
-        <input name="mes" type="hidden" value="<?php echo date("n");?>"/>
-        <input name="year" type="hidden" value="<?php echo date("Y");?>"/>
+        <input name="dia" type="hidden" value=""/>
+        <input name="mes" type="hidden" value=""/>
+        <input name="year" type="hidden" value=""/>
         <input type="submit" value="Enviar"/>
     </form>
 

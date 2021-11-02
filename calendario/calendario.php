47710862fc0bdaa6364b247eb6ca27723a6bce47
Irene González Cantera <irenegcantera@gmail.com>
2021-11-02 23:46:28 +0100

    CAL-005 Comments added

--- calendario/calendario.php
+++ calendario/calendario.php
@@ -13,6 +13,7 @@
     <?php 
     require 'funciones.php';
 	
+    /* Comprobamos si se han obtenido los valores de día, mes y año. Si es cierto se guardarán en variables y sino se obtendrá la fecha actual */
 	if(isset($_GET['dia']) && isset($_GET['mes']) && isset($_GET['year'])){
         $dia = $_GET['dia'];
         $mes = $_GET['mes'];
@@ -23,20 +24,24 @@
         $year = date("Y");
     }
 
-    crearTablaMes($dia,$mes,$year);
+    crearTablaMes($dia,$mes,$year); // Llamada a la función para crear la tabla del calendario con los valores guardados anteriormente
 	
+    /* Si se le ha dado al botón enviar y existe un valor de tareas, se guardará en una variable. Se comprueba si existe el fichero del año, mes y día, si existe se guardará las tareas en el fichero.
+    En el caso de que no se escribiera tareas y se envia, se borrará el fichero.
+    Si no se le ha dado a enviar en un principio se mostrará las tareas que se hayan enviado anteriormente.*/
     if(isset($_GET['enviar'])){
         if(isset($_GET['tareas'])){
             $texto = $_GET['tareas'];
             $nombreFichero = $year.$mes.$dia.".txt";
             if(file_exists($nombreFichero)){
                 $tareas = guardaArchivo($dia,$mes,$year, $texto);
-            }
+            } // Falta lo que pasaría si no existe el fichero
         }else{
             borraArchivo($dia,$mes,$year);
         }
     }else{
         $tareas = leeArchivo($dia,$mes,$year);
+        // Si las tareas es igual a 0, no se mostrará nada
         if($tareas == "0"){
             $tareas = null;
         }

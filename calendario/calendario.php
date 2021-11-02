da32979c18b949bce3e493afed6bf2fe16ec9866
Irene González Cantera <irenegcantera@gmail.com>
2021-11-02 23:24:02 +0100

    CAL-004 Required funciones.php, checks if there are values in , U

--- calendario/calendario.php
+++ calendario/calendario.php
@@ -4,39 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calendario de Tareas</title>
     <link rel = "stylesheet" type = "text/css" href = "estilos.css">
 </head>
 <body>
     <h1>CALENDARIO</h1>
 
     <?php 
-    require ('funciones.php');
-    crearTablaMes();
-    // if(isset($_GET['tareas'])){
-                
-    // }else{
-    //     echo "NADA";
-    // }
-    // funcion que te dice dia, mes y año actual, esto son los hidden 
-    // se pasan por parámetro en crear tabla 
-    // y se coloca en echo
-    // otra funcion que calcule mes siguiente y otra mes anterior y te lo saca solo
+    require 'funciones.php';
+	
+	if(isset($_GET['dia']) && isset($_GET['mes']) && isset($_GET['year'])){
+        $dia = $_GET['dia'];
+        $mes = $_GET['mes'];
+        $year = $_GET['year'];
+    }else{
+        $dia = date("j");
+        $mes = date("n");
+        $year = date("Y");
+    }
+	
+    crearTablaMes($dia,$mes,$year);
+	
+    if(isset($_GET['enviar'])){
+        if(isset($_GET['tareas'])){
+            $texto = $_GET['tareas'];
+            $nombreFichero = $year.$mes.$dia.".txt";
+            if(file_exists($nombreFichero)){
+                $tareas = guardaArchivo($dia,$mes,$year, $texto);
+            }
+        }else{
+            borraArchivo($dia,$mes,$year);
+        }
+    }else{
+        $tareas = leeArchivo($dia,$mes,$year);
+        if($tareas == "0"){
+            $tareas = null;
+        }
+    }
     ?>
     
     <form name = "form" action="" method="GET">
         <br>
         <strong>Citas del día:</strong>
         <br>
-        <textarea name="tareas" rows="5" cols="54" autofocus></textarea>
+        <textarea name="tareas" rows="5" cols="54" autofocus><?php echo $tareas;?></textarea>
         <br>
-        <input name="dia" type="hidden" value=""/>
-        <input name="mes" type="hidden" value=""/>
-        <input name="year" type="hidden" value=""/>
-        <input type="submit" value="Enviar"/>
-    </form>
-
-        
+        <input name="dia" type="hidden" value="<?php echo $dia;?>"/>
+        <input name="mes" type="hidden" value="<?php echo $mes;?>"/>
+        <input name="year" type="hidden" value="<?php echo $year;?>"/>
+        <input name="enviar" type="submit" value="Enviar"/>
+    </form>      
 </body>
 </html>
 

007a814729588e2901eb6c5105be6aff4a6d40d9
Irene González Cantera <irenegcantera@gmail.com>
2021-11-04 23:38:11 +0100

    AT-002 Created the table with the information from contactos.txt file. The photos can't be viewed and the operations aren't available

--- CRUD-agenda/listar.php
+++ CRUD-agenda/listar.php
@@ -1,46 +1,25 @@
-<?php include 'components/navbar.php';?>
+<?php 
+    include 'components/navbar.php';
+    require 'functions.php';
 
-<?php
-    // mostrar la tabla por pantalla
-    // echo "<table border>";
-    // echo "<tr><th>Nombre</th><th>Teléfono</th></tr>";
-    // foreach ($nombreArray as $i => $n) {
-    //     echo "<tr><td>$nombreArray[$i]</td>"; 
-    //     echo "<td>$telfArray[$i]</td></tr>";;
-    // }
-    // echo "<table>"; 
+    // MOSTRAR LA TABLA POR PANTALLA
+    echo "<br><table border='1'>";
+    echo "<tr>";
+        echo "<th>Nombre</th>";
+        echo "<th>Teléfono</th>";
+        echo "<th>Foto</th>";
+        echo "<th>Operaciones</th>";
+    echo "</tr>";
+    $datos = getContactos(); //obtener los datos
+    foreach($datos as $key => $value){
+        echo "<tr>";
+        foreach($value as $k => $v){
+            echo "<td>$v</td>";
+        }
+        echo "<td>OPERACIONES</td>";
+        echo "</tr>";
+    }
+    echo "</table><br>";
 
-//         <table border="1">
-//             <thead>
-//                 <td>Nombre</td>
-//                 <td>Teléfono</td>
-//                 <td>Foto</td>
-//                 <td>Operaciones</td>
-//             </thead>
-//             <tr>
-//                 <td>Pepito</td>
-//                 <td>66666</td>
-//                 <td><img src='Pepito.jpg' height=80 width=80/></td>
-//                 <td><a href='crear.php?editar=true&nombre=Pepito&telefono=66666'>Editar</a>
-//                     <br>
-//                     <a href='listar.php?nombre=Pepito'>Elminar</a>
-//             </td>
-//             </tr>
-//             <tr>
-//                 <td>Manolo</td><td>125468
-// </td><td><img src='Manolo.jpg' height=80 width=80/></td><td><a href='crear.php?editar=true&nombre=Manolo&telefono=125468
-// '>Editar</a><br><a href='listar.php?nombre=Manolo'>Elminar</a></td></tr><tr><td>Juan Pedro </td><td>22
-// </td><td><img src='Juan Pedro .jpg' height=80 width=80/></td><td><a href='crear.php?editar=true&nombre=Juan Pedro &telefono=22
-// '>Editar</a><br><a href='listar.php?nombre=Juan Pedro '>Elminar</a></td></tr><tr><td>aa</td><td>123
-// </td><td><img src='aa.jpg' height=80 width=80/></td><td><a href='crear.php?editar=true&nombre=aa&telefono=123
-// '>Editar</a><br><a href='listar.php?nombre=aa'>Elminar</a></td></tr><tr><td>Hola</td><td>123456789
-// </td><td><img src='Hola.jpg' height=80 width=80/></td><td><a href='crear.php?editar=true&nombre=Hola&telefono=123456789
-// '>Editar</a><br><a href='listar.php?nombre=Hola'>Elminar</a></td></tr>    </table>
-
-
-
-
+include 'components/footer.php';
 ?>
-
-
-<?php include 'components/footer.php';?>

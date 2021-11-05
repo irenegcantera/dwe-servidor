ae73c6dea28f62134961f37502780021a5874d68
Irene González Cantera <irenegcantera@gmail.com>
2021-11-05 19:45:22 +0100

    AT-005 Called the showContactos() functions

--- CRUD-agenda/listar.php
+++ CRUD-agenda/listar.php
@@ -3,23 +3,9 @@
     require 'functions.php';
 
     // MOSTRAR LA TABLA POR PANTALLA
-    echo "<br><table border='1'>";
-    echo "<tr>";
-        echo "<th>Nombre</th>";
-        echo "<th>Teléfono</th>";
-        echo "<th>Foto</th>";
-        echo "<th>Operaciones</th>";
-    echo "</tr>";
     $datos = getContactos(); //obtener los datos
-    foreach($datos as $key => $value){
-        echo "<tr>";
-        foreach($value as $k => $v){
-            echo "<td>$v</td>";
-        }
-        echo "<td>OPERACIONES</td>";
-        echo "</tr>";
-    }
-    echo "</table><br>";
+    showContactos($datos);
+    // showContactos(getContactos());
 
-include 'components/footer.php';
+    include 'components/footer.php';
 ?>

6f7bb14ddb90fce930efe6a0f476297cc857923a
Irene González Cantera <irenegcantera@gmail.com>
2021-11-06 17:01:27 +0100

    Checks if the file exists in the getContacts function and if the data is empty in the showContacts function. creates the saveContacts function to save the new data in the file and the deleteContacts function.

--- CRUD-agenda/functions.php
+++ CRUD-agenda/functions.php
@@ -15,7 +15,6 @@ function addContactos($nombre,$telf,$foto) {
         // echo "Ha ocurrido un error.";
     }
 
-    // $rutaFichero = "./files/contactos.txt"; // ruta Fichero
     // Si existe se introducirá el nombre y telefono, sino existe se creará y se introducirá
     file_put_contents(FICHERO, $nombre.";".$telf.";".$rutaFoto, FILE_APPEND); // para que no sobreescriba
     file_put_contents(FICHERO, "\n", FILE_APPEND);
@@ -24,27 +23,33 @@ function addContactos($nombre,$telf,$foto) {
 
 /* Función que lee el fichero y recupera la información guardandola en un array */
 function getContactos(){
-    // $rutaFichero = "./files/contactos.txt";
-    $fichero = file_get_contents(FICHERO);  // obtener todo el contenido
-    $linea_datos = explode("\n",$fichero); // guardar el contenido separado por un salto de línea en un array 
-    // Eliminar el último índice del array ya que se introduce un espacio en blanco
-    array_pop($linea_datos); 
+    if (file_exists(FICHERO)){
+        $fichero = file_get_contents(FICHERO);  // obtener todo el contenido
+        $linea_datos = explode("\n",$fichero); // guardar el contenido separado por un salto de línea en un array 
+        // Eliminar el último índice del array ya que se introduce un espacio en blanco
+        array_pop($linea_datos); 
 
-    $datos = array(); // nuevo array bidimensional
-    $i = 0; // contador
+        $datos = array(); // nuevo array bidimensional
+        $i = 0; // contador
 
-    foreach($linea_datos as $key => $value){
-        foreach(explode(';',$value) as $v){
-            $datos[$key][$i++] = $v;
+        foreach($linea_datos as $key => $value){
+            foreach(explode(';',$value) as $v){
+                $datos[$key][$i++] = $v;
+            }
         }
-    }
 
-    return $datos;
+        return $datos;
+    }else{
+        $fichero = fopen("./files/contactos.txt","w");
+        fclose($fichero);
+    }
+    
 }
 
 /* Función que muestra la tabla */
 function showContactos($datos){
-    echo "<br><table>
+    if(!empty($datos)){
+        echo "<br><table>
             <tr>
                 <th>Nombre</th>
                 <th>Teléfono</th>
@@ -52,28 +57,76 @@ function showContactos($datos){
                 <th>Operaciones</th>
             </tr>";
     
-    $indice = 2; // indice que sirve para mostrar la imagen en la tabla
-    $indNombre = 0; // indice para el nombre
-    $indTelf = 1; // indice para el teléfono
-    foreach($datos as $key => $value){
-        echo "<tr>";
-        foreach($value as $k => $v){
-            if ($k != $indice){ // si la clave es igual al índice, se imprime la imagen
-                echo "<td>$v</td>";
-            }else{
-                echo "<td><img src='$v' style = 'width:25%'></td>";
+        $indice = 2; // indice que sirve para mostrar la imagen en la tabla
+        $indNombre = 0; // indice para el nombre
+        $indTelf = 1; // indice para el teléfono
+        foreach($datos as $key => $value){
+            echo "<tr>";
+            foreach($value as $k => $v){
+                if ($k != $indice){ // si la clave es igual al índice, se imprime la imagen
+                    echo "<td>$v</td>";
+                }else{
+                    echo "<td><img src='$v' style = 'width:25%'></td>";
+                }
             }
-        }
 
-        echo "<td><a href = crear.php?editar=true&nombre=".$datos[$key][$indNombre]."&telefono=".$datos[$key][$indTelf].">Editar</a><a>Eliminar</a></td>"; // mostrar enlaces de Editar y Eliminar
-        echo "</tr>";
-        $indice +=3; // se suma 3 posiciones para la siguiente clave
-        $indNombre +=3; // se suma 3 posiciones 
-        $indTelf +=3; // se suma 3 posiciones 
+            echo "<td><a href = crear.php?editar=true&nombre=".$datos[$key][$indNombre]."&telefono=".$datos[$key][$indTelf].">Editar</a>
+                    <a href = listar.php?nombre=".$datos[$key][$indNombre].">Eliminar</a></td>"; // mostrar enlaces de Editar y Eliminar
+            echo "</tr>";
+            $indice +=3; // se suma 3 posiciones para la siguiente clave
+            $indNombre +=3; // se suma 3 posiciones 
+            $indTelf +=3; // se suma 3 posiciones 
+        }
+        echo "</table><br>";
+    }else{
+        echo "<p>NO HAY NINGÚN CONTACTO EN LA AGENDA</p>";
     }
-    echo "</table><br>";
+    
+}
+
+function saveContactos($datos){
+    unlink(FICHERO); // ELIMINar fichero
+    $indice = 2; // indice que sirve para mostrar la imagen en la tabla
+    foreach($datos as $key => $value){
+        foreach($value as $k => $v){
+            if ($k != $indice){
+                file_put_contents(FICHERO, $v.";", FILE_APPEND);
+            }else{
+                file_put_contents(FICHERO, $v, FILE_APPEND); // para que no se sobreescriba
+            }
+             
+        }   
+        file_put_contents(FICHERO, "\n", FILE_APPEND);
+        $indice +=3; // se suma 3 posiciones para la siguiente clave
+    }
+    
 }
 
 // deleteContacto($nombre). Elimina el contacto cuyo nombre coincida con el parámetro.
+function deleteContacto($nombre){
+    $datos = getContactos();
+    foreach($datos as $key => $value){
+        foreach($value as $k => $v){
+            if($v == $nombre){
+                array_splice($datos, $key, 1);
+                //echo "Se ha borrado";
+            }
+        }
+    }
+
+    // Reindexar las posiciones de los valores
+    $datosActualizados = array();
+    $i = 0;
+    foreach($datos as $key => $value){
+        foreach($value as $k => $v){
+            $datosActualizados[$key][$i++] = $v;
+        }
+    }
+        
+    saveContactos($datosActualizados);
+}
 // updateContacto($nomAnt,$nombre,$telefono,$foto). Actualiza el contacto cuyo nombre coincida con $nomAnt con los nuevos datos. También puedes eliminar y añadir.
+function updateContacto($nomAnt,$nombre,$telef,$foto){
+
+}
 ?>

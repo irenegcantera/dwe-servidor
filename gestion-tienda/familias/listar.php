5913bb502c6555a9e6ae9d1c2cfa94cd94c76337
Irene González Cantera <irenegcantera@gmail.com>
2021-11-13 21:18:21 +0100

    pages where the tables will be displayed

---
+++ gestion-tienda/familias/listar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once '../conf/config.inc';
+    require '../functions.php';
+    include '../parts/menu.php';
+
+    // MOSTRAR FOMRULARIO PARA LA BÚQUEDA DE FAMILIAS DE PRODUCTOS
+
+
+    // mostrar tabla, ultima columna editar y eliminar, te llevará a crear.php
+    showDatosFamilias();
+?>
+
---
+++ gestion-tienda/tiendas/listar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once '../conf/config.inc';
+    require '../functions.php';
+    include '../parts/menu.php';
+
+    // MOSTRAR FOMRULARIO PARA LA BÚQUEDA DE TIENDAS
+
+
+    // mostrar tabla, ultima columna editar y eliminas, te llevará a crear.php
+    showDatosTiendas();
+?>
+
